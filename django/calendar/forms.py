6a1050aaed224b6b78cca0245105f9766a01d1b9
Mike Allaway <m.j.allaway@bham.ac.uk>
2020-11-24 22:06:35 +0000

    added models, admin, forms

---
+++ django/calendar/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from . import models
+from captcha.fields import ReCaptchaField, ReCaptchaV3
+
+
+class AnswerCreateForm(forms.ModelForm):
+    """
+    Form to specify fields in the answer create form
+    """
+
+    story_text = forms.CharField(max_length=1000,
+                                 widget=forms.Textarea(attrs={'placeholder': 'Write your answer...'}),
+                                 label='Answer the question below')
+    captcha = ReCaptchaField(widget=ReCaptchaV3, label='')
+
+    class Meta:
+        model = models.Answer
+        fields = ('answer_text',)
