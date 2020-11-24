9a33c99d317b738c644362e146762812a45785eb
Mike Allaway <m.j.allaway@bham.ac.uk>
2020-11-24 17:07:21 +0000

    added core content and replaced from template to this project

--- django/core/local_settings.example.py
+++ django/core/local_settings.example.py
@@ -17,15 +17,15 @@ SECRET_KEY = ''
 DEBUG = True/False
 
 # Set to ['*'] if in development, or specific IP addresses and domains if in production
-ALLOWED_HOSTS = ['*']/['django-template.bham.ac.uk']
+ALLOWED_HOSTS = ['*']/['clic-calendar.bham.ac.uk']
 
 # Set the database name below
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'django-template.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'clic-calendar.sqlite3'),
         'TEST': {
-            'NAME': os.path.join(BASE_DIR, 'django-template_TEST.sqlite3'),
+            'NAME': os.path.join(BASE_DIR, 'clic-calendar_TEST.sqlite3'),
         },
     }
 }
--- django/core/wsgi.py
+++ django/core/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django-template project.
+WSGI config for clic-calendar project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
