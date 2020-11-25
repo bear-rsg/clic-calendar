95cea85b5c446a62be12b161effbfcd63422dda0
Mike Allaway <m.j.allaway@bham.ac.uk>
2020-11-25 17:22:13 +0000

    fixed answer url

--- django/calendar/urls.py
+++ django/calendar/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.QuestionListView.as_view(), name='question-list'),
     path('<int:pk>', views.QuestionDetailView.as_view(), name='question-detail'),
     # Answers
-    path('<int:pk>', views.AnswerDetailView.as_view(), name='answer-detail'),
+    path('answer/<int:pk>', views.AnswerDetailView.as_view(), name='answer-detail'),
     path('answer/', views.AnswerCreateView.as_view(), name='answer-create'),
     path('success/', views.AnswerCreateSuccessTemplateView.as_view(), name='answer-create-success'),
 ]
