d20dc2b005a8770bb04953272d2298ab2b5c4acd
Mike Allaway <36171746+mike-allaway@users.noreply.github.com>
2021-04-22 09:27:59 +0100

    Update django/researchdata/models.py
    Co-authored-by: Ed <edmondac@users.noreply.github.com>

--- django/researchdata/models.py
+++ django/researchdata/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.core.mail import send_mail
 from django.conf import settings
+import logging
 
+logger = logging.getLogger(__name__)
 
 class Year(models.Model):
     """
